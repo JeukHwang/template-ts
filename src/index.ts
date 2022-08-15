e49c6c830d843c114e123cccb7664fa6c038326a
JeukHwang <92910273+JeukHwang@users.noreply.github.com>
2022-08-16 01:45:08 +0900

    feat, style: add code to test config

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
 import { UserAccount } from "./userAccount";
 
 const user = new UserAccount("JeukHwang", 1);
+
+const { id, name } = user.toSequalize();
 // eslint-disable-next-line no-console
-console.log(user);
+console.log(id + 1);
+
+// eslint-disable-next-line no-console
+console.log(name.toLowerCase());
+
+// eslint-disable-next-line no-console
+console.log(name.trim().length);
--- src/userAccount.ts
+++ src/userAccount.ts
@@ -1,8 +1,12 @@
 export class UserAccount {
     name: string;
     id: number;
-    constructor(name:string, id:number) {
+    constructor(name: string, id: number) {
         this.name = name;
         this.id = id;
+    }
+
+    toSequalize(): { name: string; id: number } {
+        return { name: this.name, id: this.id };
     }
 }
